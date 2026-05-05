54ddcf7747c81d87b3b425c62f129e565ed04a2f
FilmakerTom <tomsig.video@gmail.com>
2026-05-05 12:36:46 +0200

    Update MenuSection.tsx

--- src/components/MenuSection.tsx
+++ src/components/MenuSection.tsx
@@ -1,6 +1,7 @@
 import { useScrollReveal } from "@/hooks/use-scroll-reveal";
 
 const pizzas = [
+  { name: "Asparagi verdi, cotto affumicato e bufala", description: "Crema di asparagi verdi, prosciutto cotto affumicato, bufala campana, senape in grani", price: "€14" },
   { name: "Speck Carciofi e Burrata", description: "Fiordilatte, speck tirolese, carciofi a spicchi, burrata, polvere di peperone di Senise", price: "€15" },
   { name: "Carbonara d'Asparagi", description: "Fiordilatte, asparagi verdi al forno, guanciale, carbocrema, pecorino", price: "€15" },
 ];
